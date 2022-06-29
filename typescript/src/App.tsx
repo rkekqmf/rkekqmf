ccff13ad0788a2ad30910b40332c12412f667c1f
rkekqmf <rkekqmf@gmail.com>
2022-06-29 14:41:41 +0900

    add : ts basic

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const a: string = "123";
+  const b: number = 123;
+  const c: boolean = true;
+  const d: undefined = undefined;
+  const e: null = null;
+  console.log(a, b, c, d, e);
+
+  const f: string[] | number[] = ["123", "123"];
+  const g: [string, number] = ["123", 123];
+  console.log(f, g);
+
+  const h: { hh: string; hhh: string } = { hh: "123", hhh: "12" };
+  console.log(h);
+
   type Name = string[] | number;
   const nameYong: Name = 123;
 
