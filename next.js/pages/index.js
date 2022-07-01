444bbfbe59e60052b385d860d816f123c70d5363
rkekqmf <rkekqmf@gmail.com>
2022-07-02 00:19:11 +0900

    add : server side rendering

--- next.js/pages/index.js
+++ next.js/pages/index.js
@@ -1,25 +1,34 @@
 import Seo from "../components/Seo";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
+  // const [movies, setMovies] = useState();
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (await fetch(`/api/movies`)).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
 
   return (
     <>
       <Seo title="홈" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {!results && <h4>Loading...</h4>}
+      {results?.map((movie) => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
       ))}
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+
+  return {
+    props: {
+      results,
+    },
+  };
 }
